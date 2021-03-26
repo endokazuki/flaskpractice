3f7f155003ac8b413d1292e817f16d8438c3fdc6
endokazuki <53686299+endokazuki@users.noreply.github.com>
2021-03-26 17:41:54 +0900

    Delete 20210312/application directory

--- 20210312/application/flask_blog/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-#Flaskを使う
-
-app = Flask(__name__)
-
-import flask_blog.views
-#処理命令をviews.pyに受け渡す
--- 20210312/application/flask_blog/views.py
+++
@@ -1,5 +0,0 @@
-from flask_blog import app
-
-@app.route('/')
-def show_entries():
-    return "hello world!"
